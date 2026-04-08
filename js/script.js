d9ee678604e42e6795372200111e0494c1ce3c79
Rafael Arantes <rafaelarantesg@gmail.com>
2026-04-07 22:53:45 -0300

    att artes

--- js/script.js
+++ js/script.js
@@ -105,3 +105,19 @@ function animateParticles() {
 }
 
 animateParticles();
+
+// =========================
+// MODAL DE IMAGEM
+// =========================
+
+function openModal(src) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+
+    modal.style.display = "flex";
+    modalImg.src = src;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
